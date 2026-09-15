26918e5459b35123be763ccc4c9e810e9b6deef2
ZhihengZaneXu <zanexu@proton.me>
2026-09-15 17:29:31 -0400

    add format

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -2,16 +2,23 @@ return {
 	{
 		"stevearc/conform.nvim",
 		config = function()
+			-- prettierd (fast, stays running) when installed, otherwise prettier.
+			local prettier = { "prettierd", "prettier", stop_after_first = true }
+
 			require("conform").setup({
 				formatters_by_ft = {
 					lua = { "stylua" }, -- Lua formatter
 					python = { "black" }, -- Python formatter
-					javascript = { "prettier" }, -- JavaScript formatter
-					typescript = { "prettier" }, -- TypeScript formatter
+					javascript = prettier, -- JavaScript formatter
+					typescript = prettier, -- TypeScript formatter
+					css = prettier,
+					html = prettier,
+					json = prettier,
 					tex = { "latexindent" }, -- LaTeX formatter
+					-- No markdown: Limesurvey_user.md has one-line HTML that must not be re-wrapped.
 				},
 				format_on_save = {
-					timeout_ms = 500, -- Set timeout for format-on-save
+					timeout_ms = 2000, -- Prettier's startup alone can exceed 500 ms on large files
 				},
 			})
 
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/lua/plugins/snacks.lua
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		terminal = { enabled = true },
+	},
+}
